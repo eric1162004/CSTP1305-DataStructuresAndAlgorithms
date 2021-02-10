d45dd23e08747f9aebbeee934f6029383a9f678f
Eric Cheung <000437017@student.vcc.ca>
2021-02-09 16:18:17 -0800

    corrected java test cases in the java code

--- Assignment1/Java codes/Main.java
+++ Assignment1/Java codes/Main.java
@@ -57,7 +57,6 @@ public class Main {
         System.out.printf("Number of occurrence of 4: %s\n", aQueue.getFrequency(4));
         System.out.printf("Is the queue empty now: %s\n", aQueue.isEmpty());*/
 
-
 /*      QueueUsingLists aQueue =  new QueueUsingLists();
         aQueue.insert(6);
         aQueue.insert(5);
@@ -74,49 +73,6 @@ public class Main {
         aQueue.clear();
         System.out.printf("Number of occurrence of 4: %s\n", aQueue.getFrequency(4));
         System.out.printf("Is the queue empty now: %s\n", aQueue.isEmpty());*/
-
-/*        PriorityQueueUsingLists aQueuelist =  new PriorityQueueUsingLists();
-        aQueuelist.insert(6);
-        aQueuelist.insert(4);
-        aQueuelist.insert(7);
-        aQueuelist.insert(3);
-        aQueuelist.remove();        // which item should be served if its data represent its priority (the higher the data, the higher the priority): 7
-        aQueuelist.remove();        // remove 6
-        aQueuelist.insert(9);
-        aQueuelist.insert(5);
-        // some debugging // front point to 7 and its value = 2
-        // back points to 5 and values = 0
-        System.out.println(aQueuelist.peek());*/
-
-/*        QueueUsingCircularList aQueuelist =  new QueueUsingCircularList();
-        aQueuelist.insert(6);
-        aQueuelist.insert(4);
-        aQueuelist.insert(7);
-        aQueuelist.insert(3);
-        aQueuelist.remove();        // which item should be served if its data represent its priority (the higher the data, the higher the priority): 7
-        aQueuelist.remove();        // remove 6
-        aQueuelist.insert(9);
-        aQueuelist.insert(5);
-        // some debugging // front point to 7 and its value = 2
-        // back points to 5 and values = 0
-        System.out.println(aQueuelist.peek());*/
-
-/*        QueueUsingDoubleList aQueuelist =  new QueueUsingDoubleList();
-        aQueuelist.insertAtBack(10);
-        aQueuelist.insertAtBack(4);
-        aQueuelist.insertAtBack(2);
-        aQueuelist.insertAtBack(5);
-        aQueuelist.removeFromBack();
-        System.out.println(aQueuelist.peekFromBack()); //2*/
-
-        int arr[] = new int[10];
-        for(int i =0; i< arr.length; i++)
-            arr[i] = i;
-
-        Searching s = new Searching();
-        int result = s.linear_search(arr, 111);
-        result = s.binary_search_iterative(arr, 2);
-        System.out.println(result);
 
     }
 }
