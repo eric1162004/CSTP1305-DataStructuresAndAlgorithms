31bb75635172f26a7efa337bfad036caaee63b57
Eric Cheung <000437017@student.vcc.ca>
2021-03-31 01:41:31 -0700

    Added benchMarking

--- Assignment3/src/Main.java
+++ Assignment3/src/Main.java
@@ -1,41 +1,75 @@
+import java.lang.reflect.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        int [] array = {4,5,6,3,54,5,8,3,1};
-        int [] array1 = {4,5,6,3,54,5,8,3,1};
-        int [] array2 = {4,5,6,3,54,5,8,3,1};
-        int [] array3 = {4,5,6,3,54,5,8,3,1};
 
         var selectionSort = new SelectionSort();
-        selectionSort.sort(array);
-        print(array);
-        print(null);
-
         var heapSort = new HeapSort();
-        heapSort.sort(array1);
-        print(array1);
-        print(null);
-
         var insertionSort = new InsertionSort();
-        insertionSort.sort(array2);
-        print(array2);
-        print(null);
-
         var mergeSort = new MergeSort();
-        mergeSort.sort(array3);
-        print(array3);
+
+        var numberOfRuns = 1000;
+
+        Thread t1 = new Thread(() -> benchMark(selectionSort::sort, "Selection Sort", numberOfRuns));
+        Thread t2 = new Thread(() -> benchMark(heapSort::sort, "Heap Sort", numberOfRuns));
+        Thread t3 = new Thread(() -> benchMark(insertionSort::sort, "Insertion Sort", numberOfRuns));
+        Thread t4 = new Thread(()->benchMark(mergeSort::sort, "Merge Sort",numberOfRuns));
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
 
     }
 
-    private static void print(int[] array){
-        if(array == null) {
-            System.out.println();
-            return;
+    public static void benchMark(ISortArray method, String methodName, int numberOfRuns)
+    {
+        var results = new long[numberOfRuns];
+        long startTime;
+        long endTime;
+
+        for (int i = 0; i < numberOfRuns; i++) {
+            var array = generateRandomNumbers(10000, 1000);
+
+            // System.out.println("unsorted" + Arrays.toString(array));
+
+            startTime = System.currentTimeMillis();
+            method.sort(array);
+            endTime = System.currentTimeMillis();
+
+            // System.out.println("sorted" + Arrays.toString(array));
+
+            results[i] = endTime-startTime;
         }
 
+        var average = Arrays.stream(results).average().getAsDouble();
+        var total = Arrays.stream(results).sum();
+
+/*        System.out.println(methodName + " execution stats:");
+        System.out.println("=================================");
+        System.out.println("Number of Runs: " + numberOfRuns);
+        System.out.println("Average execution time: " + average + "ms");*/
+        System.out.println(methodName + " total execution time: " + total + "ms");
+    }
+
+    public static int[] generateRandomNumbers(int size, int upperRange){
+        Random rd = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Math.abs(rd.nextInt() % upperRange);
+        }
+        return arr;
+    }
+
+    private static void print(int[] array){
         for (var i :
                 array) {
             System.out.print(i + " ");
         }
+        System.out.println();
     }
 
 }
